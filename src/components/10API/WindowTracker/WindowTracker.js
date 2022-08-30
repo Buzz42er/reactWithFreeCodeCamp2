b90c6f525dc66e29e8ad709ace6233d03cc8390d
Buzz <jure.eres42@gmail.com>
2022-08-30 13:55:48 +0200

    finished 3 section

--- src/components/10API/WindowTracker/WindowTracker.js
+++ src/components/10API/WindowTracker/WindowTracker.js
@@ -10,9 +10,15 @@ const WindowTracker = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    window.addEventListener("resize", function () {
-      setWindowWidth((prevWindowWidth) => window.innerWidth);
-    });
+    function widthChange() {
+      setWindowWidth((prevWindowWidth) => window.innerWidth); //renders new value inside return of WindowTracker, evertime window width changes
+    }
+
+    window.addEventListener("resize", widthChange); //listens to changes in width
+
+    return () => {
+      window.removeEventListener("resize", widthChange); //it's executed right before it's not rendered enymore
+    };
   }, []);
 
   return <h1>Window width: {windowWidth}</h1>;
--- src/components/4meme-generator/MemeMain.js
+++ src/components/4meme-generator/MemeMain.js
@@ -104,9 +104,14 @@ const MemeMain = () => {
   const [allMemeImages, setAllMemeImages] = useState([]);
 
   useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then((res) => res.json())
-      .then((data) => setAllMemeImages(data.data.memes));
+    async function getMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setAllMemeImages(data.data.memes);
+    }
+    getMemes();
+    //without cleanup
+    //cleanup is return function at the end of useEffect that runs when whole component changes
   }, []);
 
   function getMemeImage() {
